afa2af3df33e52f15a0dcad30664fa5ac49437b2
Tim Nguyen <frenchfryfan@users.noreply.github.com>
2025-04-22 10:10:20 -0700

    Update drive-time.js

--- api/drive-time.js
+++ api/drive-time.js
@@ -30,7 +30,7 @@ export default async function handler(req, res) {
     const duration = route.legs[0].duration.text;
     const polyline = route.overview_polyline?.points;
 
-    const mapUrl = `${staticMapBaseUrl}?size=600x300&path=enc:${polyline}&markers=color:green|${HOME_ADDRESS}&markers=color:red|${WORK_ADDRESS}&key=${params.key}`;
+    const mapUrl = `${staticMapBaseUrl}?size=600x300&path=enc:${polyline}&markers=color:green|${process.env.HOME_ADDRESS}&markers=color:red|${process.env.WORK_ADDRESS}&key=${params.key}`;
 
     res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
     res.setHeader('Pragma', 'no-cache');
